e1a1b663be654bfa86e17970468d0900c823f9ec
Jonathan <jonathan.navas9@hotmail.com>
2020-05-19 12:25:17 -0500

    Rest usuarios

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,20 @@
             PUERTO
 ==============================*/
 process.env.PORT = process.env.PORT || 3000;
+
+
+/*=============================
+            ENTORNO
+==============================*/
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+/*=============================
+        BASE DE DATOS
+==============================*/
+let urlDB;
+if (process.env.NODE_ENV == 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://jncode:fBjKaqYCWtlpHdnr@cluster0-tfclq.mongodb.net/cafe';
+}
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
+//password mongo fBjKaqYCWtlpHdnr
+//user jncode
+//url de conexion mongodb+srv://jncode:fBjKaqYCWtlpHdnr@cluster0-tfclq.mongodb.net/cafe
 require('./config/config');
 
 const express = require('express')
+const mongoose = require('mongoose');
 
-const app = express()
+const app = express();
 
 const bodyParser = require('body-parser')
     // parse application/x-www-form-urlencoded
@@ -11,41 +15,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(require('./routes/usuario'));
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario');
-});
-
-app.post('/usuario', function(req, res) {
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es obligatorio'
-        });
-
-    } else {
-
-    }
-
-    res.json({
-        persona: body
-    });
-});
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
+mongoose.connect(process.env.URLDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}, (err, resp) => {
+    if (err) throw err;
+    console.log('Conexion exitosa');
 });
 
 app.listen(process.env.PORT, () => {
