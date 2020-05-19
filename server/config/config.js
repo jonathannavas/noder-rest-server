6fbe7ff48fb2cec3ee33f70df6885e2ae4791749
Jonathan <jonathan.navas9@hotmail.com>
2020-05-19 13:10:36 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,6 @@ let urlDB;
 if (process.env.NODE_ENV == 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://jncode:fBjKaqYCWtlpHdnr@cluster0-tfclq.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
